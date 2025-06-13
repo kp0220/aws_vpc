4eba0d437b37d5345cf76b6405a075378abf798f
Kavit Patel <kavit1986@gmail.com>
2025-06-13 22:30:06 +0530

    file updated

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
