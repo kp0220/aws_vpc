df2061aad84f5ab01d96754060e1cd89664f14ca
Kavit Patel <kavit1986@gmail.com>
2025-06-13 22:23:12 +0530

    File changes

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,16 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "public_subnet1_id" {
+  value = aws_subnet.public1.id
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "private_subnet1_id" {
+  value = aws_subnet.private1.id
+}
+
+output "private_subnet2_id" {
+  value = aws_subnet.private2.id
 }
 
 output "internet_gateway_id" {
--- vpc.tf
+++ vpc.tf
@@ -91,35 +91,3 @@ resource "aws_route_table_association" "private2" {
   subnet_id      = aws_subnet.private2.id
   route_table_id = aws_route_table.private.id
 }
-
-# security_groups.tf
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
-}
